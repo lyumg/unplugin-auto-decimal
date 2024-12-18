a018f3639cb9f93a5b8d2f750b85371c876f571d
lvming <lvming@zztabc.com>
2024-12-18 10:49:16 +0800

    config: 移除 example 校验

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   type: 'lib',
-  ignores: ['**/*.md'],
+  ignores: ['**/*.md', 'examples/*'],
   rules: {
     'ts/explicit-function-return-type': 'off',
     'unicorn/consistent-function-scoping': 'off',
